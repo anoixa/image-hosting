c3ce71abdc2dcb93cd05edb75fd72daf023c795f
shuomc <singjunyu@gmail.com>
2024-09-21 13:17:21 +0800

    update 设计用户权限

---
+++ image-hosting-common/src/main/java/moe/imtop1/imagehosting/common/entity/Permissions.java
@@ -0,0 +1,49 @@
+package moe.imtop1.imagehosting.common.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+/**角色表
+ * @author shuomc
+ */
+@TableName("permissions")
+public class Permissions {
+    @TableId(type = IdType.ASSIGN_ID)
+    private String permissionId;
+    
+    private String permissionName;
+    private String description;
+
+    public Permissions() {}
+    
+    public Permissions(String permissionId, String permissionName, String description) {
+        this.permissionId = permissionId;
+        this.permissionName = permissionName;
+        this.description = description;
+    }
+
+    public String getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(String permissionId) {
+        this.permissionId = permissionId;
+    }
+
+    public String getPermissionName() {
+        return permissionName;
+    }
+
+    public void setPermissionName(String permissionName) {
+        this.permissionName = permissionName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
