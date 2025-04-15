7828eb4b0cbcbb1af727d2c7ffe7db5b9b7b42ce
shuomc <singjunyu@gmail.com>
2025-04-15 22:10:07 +0800

    [add] 图片流DTO

---
+++ image-hosting-images/src/main/java/moe/imtop1/imagehosting/images/domain/dto/ImageStreamData.java
@@ -0,0 +1,26 @@
+package moe.imtop1.imagehosting.images.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.InputStream;
+
+/**
+ * @author shuomc
+ * @Date 2025/4/15
+ * @Description
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImageStreamData {
+
+    private InputStream inputStream;  // MinIO 图片输入流
+
+    private String contentType;
+
+    private Integer size;
+
+    private String fileName;
+}
